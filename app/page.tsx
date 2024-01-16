2fc86cd93a3ffa5a9c0985bfcd8a05ff20482ad1
DrawingNea <lcy.li@outlook.de>
2024-01-16 17:15:38 +0100

    feat: get user and projects on load of home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import Image from 'next/image'
+"use client";
+import { fetchProjectShifts, fetchUser } from "@/lib/actions";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <main>
-      FE Features
-    </main>
-  )
+  const [contact, setContact] = useState(null);
+  const [projects, setProjects] = useState(null);
+  async function getUser() {
+    const { contact } = await fetchUser();
+    console.log(contact);
+    setContact(contact);
+  }
+  async function getProjects() {
+    const projects = await fetchProjectShifts();
+    console.log(projects);
+    setProjects(projects);
+  }
+  useEffect(() => {
+    getUser();
+    getProjects();
+  }, []);
+  return <main>FE Features</main>;
 }
