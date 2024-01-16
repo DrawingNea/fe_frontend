9e4c86a0ef1194cc486541c01d3679534997f0ae
DrawingNea <lcy.li@outlook.de>
2024-01-16 17:15:07 +0100

    feat: create methods to fetch projects and user

---
+++ lib/actions.ts
@@ -0,0 +1,29 @@
+"use server";
+
+export const fetchUser = async () => {
+  try {
+    const response = await fetch("http://localhost:3004/me");
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error("Error fetching user");
+    }
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
+
+export const fetchProjectShifts = async () => {
+  try {
+    const response = await fetch("http://localhost:3004/project_shifts");
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error("Error fetching project shifts");
+    }
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
