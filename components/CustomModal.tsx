cd6b22621a9f174f20cb74e98a83558934648544
DrawingNea <lcy.li@outlook.de>
2024-01-16 22:38:57 +0100

    feat: add content and animations to custom modal

--- components/CustomModal.tsx
+++ components/CustomModal.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client";
+import React, { Fragment, ReactNode } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { CustomModalProps } from "@/types";
 
-const CustomModal = () => {
+const CustomModal = ({
+  children,
+  containerStyles,
+  isOpen,
+  closeModal,
+}: CustomModalProps) => {
   return (
-    <div>CustomModal</div>
-  )
-}
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              {/*<Dialog.Overlay className="fixed inset-0" />*/}
+              <Dialog.Panel className={`relative bg-white rounded-3xl px-16 py-10 overflow-hidden shadow-xl transform transition-all ${containerStyles}`}>
+                {children}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
 
-export default CustomModal
+export default CustomModal;
