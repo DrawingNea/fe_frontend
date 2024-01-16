1c662f7bb92480092ef921b58e7caaf6d4f74068
DrawingNea <lcy.li@outlook.de>
2024-01-16 22:05:50 +0100

    style: change cards border roundness, make card title bigger and remove margins from button

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -15,10 +15,10 @@ const ProjectCard = ({
   return (
     <div
       className=" block bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.1),0_10px_20px_-2px_rgba(0,0,0,0.1)]
-    flex-1 w-[80%] max-h-72 mb-5 py-10 text-center rounded-2xl"
+    flex-1 w-[80%] max-h-72 mb-5 py-10 text-center rounded-3xl"
     >
       <Link href={`/project-shift/${id}`}>
-        <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 ">
+        <h5 className="mb-2 text-2xl font-medium leading-tight text-neutral-800 ">
           {position}
         </h5>
       </Link>
@@ -41,7 +41,7 @@ const ProjectCard = ({
       </div>
       <button
         className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300
-      font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 "
+      font-medium rounded-lg text-sm px-5 py-2.5"
       >
         Get this Job
       </button>
