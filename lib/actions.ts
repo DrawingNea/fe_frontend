a87fa6d8adce9856480dbc3ac928f723397a5d9f
DrawingNea <lcy.li@outlook.de>
2024-01-16 19:29:53 +0100

    feat: add method to fetch project applications

--- lib/actions.ts
+++ lib/actions.ts
@@ -27,3 +27,19 @@ export const fetchProjectShifts = async () => {
     console.log("Error:", error);
   }
 };
+
+export const fetchProjectAppliations = async () => {
+  try {
+    const response = await fetch(
+      "http://localhost:3004/project_shift_applications"
+    );
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error("Error fetching project applications");
+    }
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
