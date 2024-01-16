9a2c0b56977f6a3fc7f769cb966d08a84db6461e
DrawingNea <lcy.li@outlook.de>
2024-01-16 19:27:34 +0100

    feat: filter projects out based on skills and sort them by start asc

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import ProjectCard from "@/components/ProjectCard";
 import { fetchProjectShifts, fetchUser } from "@/lib/actions";
-import { UserInterface,ProjectInterface, ProjectCardProps } from "@/types";
+import { UserInterface, ProjectInterface, ProjectCardProps } from "@/types";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -9,24 +9,35 @@ export default function Home() {
   const [projects, setProjects] = useState<ProjectInterface[]>([]);
   async function getUser() {
     const { contact } = await fetchUser();
-    console.log(contact);
     setContact(contact);
-    localStorage.setItem("contact-id", contact.id)
+    localStorage.setItem("contact-id", contact.id);
   }
   async function getProjects() {
     const projects = await fetchProjectShifts();
-    console.log(projects);
     setProjects(projects);
   }
   useEffect(() => {
     getUser();
     getProjects();
   }, []);
+
+  function filterProjectsBySkills(projects: ProjectInterface[]) {
+    return projects.filter((project) =>
+      project.skills.every((projectSkill) =>
+        contact!.skills.some((userSkill) => userSkill.id === projectSkill.id)
+      )
+    );
+  }
+
   return (
     <main className="grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols-1 w-full pt-14 place-items-center">
-      {projects.map((project: ProjectCardProps) => {
-        return <ProjectCard {...project} />;
-      })}
+      {filterProjectsBySkills(projects)
+        .sort(
+          (a, b) => new Date(a.start).getTime() - new Date(b.start).getTime()
+        )
+        .map((project: ProjectCardProps) => {
+          return <ProjectCard {...project} />;
+        })}
     </main>
   );
 }
