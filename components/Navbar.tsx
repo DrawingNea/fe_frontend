215f70f6970cd8b8a8c3dc3bb9347d85c2df4ece
DrawingNea <lcy.li@outlook.de>
2024-01-17 15:33:04 +0100

    feat: adjust navbar to check if user is logged in or not

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,25 +4,35 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
+  const userId = localStorage.getItem("contact-id");
   return (
     <header className="w-full z-10 sticky top-0 bg-gray-950 text-white shadow-md">
       <nav className="max-w-[1440px] mx-auto sm:px-16 px-6 py-4 flex justify-between items-center">
-          <Link href="/">
-            <Image
-              src="/vercel.svg"
-              width={115}
-              height={43}
-              alt="logo"
-              className="object-contain invert"
-            />
-          </Link>
-          <ul className=" flex gap-7 text-small items-center">
-            {NavLinks.map((link) => (
-              <Link href={link.href!} key={link.key} className="">
-                {link.text}
-              </Link>
-            ))}
-          </ul>
+        <Link href="/">
+          <Image
+            src="/vercel.svg"
+            width={115}
+            height={43}
+            alt="logo"
+            className="object-contain invert"
+          />
+        </Link>
+        <ul className=" flex gap-7 text-small items-center">
+          {NavLinks.map((link) => (
+            <Link href={link.href!} key={link.key} className="">
+              {link.text}
+            </Link>
+          ))}
+          {userId ? (
+            <Link href={`/profile/${userId}`} key={userId} className="">
+              My Profile
+            </Link>
+          ) : (
+            <Link href={`/`} key="Sign In" className="">
+              Sign In
+            </Link>
+          )}
+        </ul>
       </nav>
     </header>
   );
--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,3 @@
 export const NavLinks = [
     { href:"/", key: "Find Jobs", text: "Find Jobs" },
-    { href:"/", key: "My Profile", text: "My Profile" },
   ]
