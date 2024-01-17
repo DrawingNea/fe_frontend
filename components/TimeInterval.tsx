b720736152245cf1a806b8cc57bd6624e81d4770
DrawingNea <lcy.li@outlook.de>
2024-01-17 11:51:58 +0100

    feat: init time interval to separate dates from project card & project page

---
+++ components/TimeInterval.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TimeInterval = () => {
+  return (
+    <div>TimeInterval</div>
+  )
+}
+
+export default TimeInterval
--- components/index.tsx
+++ components/index.tsx
@@ -2,5 +2,6 @@ import Navbar from "./Navbar";
 import CustomButton from "./CustomButton";
 import CustomModal from "./CustomModal";
 import ApplicationModal from "./ApplicationModal";
+import TimeInterval from "./TimeInterval";
 
-export { Navbar, CustomButton, CustomModal, ApplicationModal };
+export { Navbar, CustomButton, CustomModal, ApplicationModal, TimeInterval };
