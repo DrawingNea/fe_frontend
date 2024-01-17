89ce5b69f6e1d92511e8be1892875a5da51f75d2
DrawingNea <lcy.li@outlook.de>
2024-01-17 11:32:20 +0100

    feat. init application modal to separate from project card

---
+++ components/ApplicationModal.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ApplicationModal = () => {
+  return (
+    <div>ApplicationModal</div>
+  )
+}
+
+export default ApplicationModal
--- components/index.tsx
+++ components/index.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import CustomButton from "./CustomButton";
 import CustomModal from "./CustomModal";
+import ApplicationModal from "./ApplicationModal";
 
-export { Navbar, CustomButton, CustomModal  }
+export { Navbar, CustomButton, CustomModal, ApplicationModal };
