9238618da7bde18b0dd8d08a20b017ef4ee30970
DrawingNea <lcy.li@outlook.de>
2024-01-17 10:39:58 +0100

    refactor: add component export to index

--- components/index.tsx
+++ components/index.tsx
@@ -1,3 +1,5 @@
 import Navbar from "./Navbar";
+import CustomButton from "./CustomButton";
+import CustomModal from "./CustomModal";
 
-export { Navbar }
+export { Navbar, CustomButton, CustomModal  }
