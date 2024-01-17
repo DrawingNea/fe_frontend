efb1d857a60bdeb5a49a957b988362df7678efa5
DrawingNea <lcy.li@outlook.de>
2024-01-17 22:29:28 +0100

    feat: add content for profile id page

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+"use client";
+
+import { DetailsPage } from "@/components";
+import { fetchUser } from "@/lib/actions";
+import { UserInterface } from "@/types";
+import React, { useEffect, useState } from "react";
 
 const ProfileDetails = () => {
-  return (
-    <div>ProfileDetails</div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState<UserInterface>();
+  async function fetchData() {
+    const {contact} = await fetchUser();
+    setUser(contact);
+  }
 
-export default ProfileDetails
+  useEffect(() => {
+    fetchData();
+    setIsLoading(false);
+  }, []);
+  return isLoading ? (
+    <></>
+  ) : (
+    <DetailsPage
+      mainTitle={user?.firstName || ""}
+      subTitle={user?.lastName || ""}
+      skills={user?.skills || [{ id: "", name: "" }]}
+    />
+  );
+};
+
+export default ProfileDetails;
