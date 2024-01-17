b61e4e4226c8d4c9fdf253286f67b0d9eac35826
DrawingNea <lcy.li@outlook.de>
2024-01-17 22:03:44 +0100

    feat: init details page

--- components/index.tsx
+++ components/index.tsx
@@ -3,5 +3,6 @@ import CustomButton from "./CustomButton";
 import CustomModal from "./CustomModal";
 import ApplicationModal from "./ApplicationModal";
 import TimeInterval from "./TimeInterval";
+import DetailsPage from "./DetailsPage";
 
-export { Navbar, CustomButton, CustomModal, ApplicationModal, TimeInterval };
+export { Navbar, CustomButton, CustomModal, ApplicationModal, TimeInterval, DetailsPage };
