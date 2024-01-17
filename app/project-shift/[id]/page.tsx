050cd62047e918335f4f2f4f1e36e00b0b9b959e
DrawingNea <lcy.li@outlook.de>
2024-01-17 22:29:11 +0100

    refactor: separate content from project id page to details page

--- app/project-shift/[id]/page.tsx
+++ app/project-shift/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { TimeInterval } from "@/components";
+import { DetailsPage, TimeInterval } from "@/components";
 import { fetchProjectShiftById } from "@/lib/actions";
 import { ProjectInterface } from "@/types";
 import React, { useEffect, useState } from "react";
@@ -19,19 +19,20 @@ const ProjectDetails = ({ params: { id } }: { params: { id: string } }) => {
   return isLoading ? (
     <></>
   ) : (
-    <div className="flex flex-col justify-center items-center place-content-center my-20">
-      <h1 className="text-5xl font-bold">{project?.position}</h1>
-      <p className="text-3xl font-medium mb-5">{project?.area}</p>
-      <p className="text-xl font-light">{project?.task}</p>
-      <TimeInterval start={project?.start || ''} end={project?.end || ''} textStyles="text-xl font-light" />
-      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800 my-5">
-        {project?.skills.map((skill) => (
-          <li className="bg-white border border-black/[0.1] rounded-xl px-5 py-3">
-            <p className="text-xl font-light capitalize">{skill.name}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <DetailsPage
+      mainTitle={project?.position || ""}
+      subTitle={project?.area || ""}
+      skills={project?.skills || [{ id: "", name: "" }]}
+    >
+      <>
+        <p>{project?.task}</p>
+        <TimeInterval
+          start={project?.start || ""}
+          end={project?.end || ""}
+          textStyles="text-xl font-light"
+        />
+      </>
+    </DetailsPage>
   );
 };
 
