bc4d1ca866adf854eebb2b4cdc11b16ba421d64c
DrawingNea <lcy.li@outlook.de>
2024-01-17 11:41:57 +0100

    refactor: separate application modal into own component

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -2,7 +2,7 @@ import { postProjectApplication } from "@/lib/actions";
 import { ProjectCardProps } from "@/types";
 import Link from "next/link";
 import React, { useState } from "react";
-import { CustomButton, CustomModal } from ".";
+import { ApplicationModal, CustomButton, CustomModal } from ".";
 
 const ProjectCard = ({
   id,
@@ -16,7 +16,6 @@ const ProjectCard = ({
 }: ProjectCardProps) => {
   const userId = localStorage.getItem("contact-id");
   const [isOpen, setIsOpen] = useState(false);
-  const [applicationMessage, setApplicationMessage] = useState("");
   return (
     <div
       className=" block bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.1),0_10px_20px_-2px_rgba(0,0,0,0.1)]
@@ -53,46 +52,13 @@ const ProjectCard = ({
       >
         Get this Job
       </button>
-      <CustomModal
+      <ApplicationModal
         isOpen={isOpen}
-        closeModal={() => setIsOpen(false)}
-        containerStyles=""
-      >
-        <h1 className="text-3xl mt-10 font-normal capitalize mb-5">
-          Your application for <span className="font-bold">{position}</span>
-        </h1>
-        <p className="text-xl">Enter your letter of application below.</p>
-        <label
-          htmlFor="message"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Your message
-        </label>
-        <textarea
-          id="message"
-          rows={4}
-          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-          placeholder="Dear Mr. Smith, ..."
-          value={applicationMessage}
-          onChange={(e) => setApplicationMessage(e.target.value)}
-        />
-        <CustomButton
-          title="Send"
-          btnType="button"
-          containerStyles="bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300
-    font-medium rounded-lg text-sm px-5 py-2.5 mt-5"
-          textStyles="text-white text-sm font-medium"
-          handleClick={(e) => {
-            postProjectApplication({
-              shift: id,
-              contact: userId!,
-              note: applicationMessage,
-            });
-            setIsOpen(false);
-            removeProject(id);
-          }}
-        />
-      </CustomModal>
+        setIsOpen={setIsOpen}
+        position={position}
+        removeProject={removeProject}
+        id={id}
+      />
     </div>
   );
 };
